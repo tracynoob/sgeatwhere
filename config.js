ddddf097ff28902b81bb79b940b34a8444ed008d
tracynoob <162657107+tracynoob@users.noreply.github.com>
2024-05-21 05:33:37 +0000

    Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+export const GOOGLE_MAPS_API_KEY = "AIzaSyCkoJecrZnsRn9jZrr2H65E-0pC3yumh30";
+
+export const GOOGLE_MAPS_URL =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyCkoJecrZnsRn9jZrr2H65E-0pC3yumh30&loading=async&libraries=places&callback=initMap";
+{
+  /* <script
+async
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkoJecrZnsRn9jZrr2H65E-0pC3yumh30&loading=async&libraries=places&callback=initMap" */
+}
---
+++ main.js
@@ -0,0 +1,281 @@
+// import { GOOGLE_MAPS_API_KEY } from "./config.js";
+
+// // import { supabase, supabaseUrl, supabaseKey } from "./supabase.js";
+// let apiKey = GOOGLE_MAPS_API_KEY;
+// // console.log(supabaseUrl);
+// import {
+//   buonaVistaData,
+//   clementiData,
+//   queenstownData,
+//   brasBasahData,
+// } from "./markerData.js";
+// import { clearFoodOptions, showFoodOptions } from "./foodOptions.js";
+
+// let map = new google.maps.Map(
+//   document.getElementById("google-map"),
+//   mapOptions
+// );
+var map;
+let markers = [];
+let newMarker;
+
+// let for newMarker:
+// let declares a block-scoped variable, which means it's only accessible within the block in which it's defined.
+// newMarker is intended to be used within the initMap and addMarker functions. Using let ensures that it's scoped to these functions and not accessible outside of them.
+// Additionally, using let instead of var allows for better code readability and maintainability, as it limits the scope of newMarker to where it's actually needed.
+
+//  Coordinates for MRT stations
+const buonaVistaMRT = { lat: 1.3074177591198148, lng: 103.78984059533414 };
+const clementiMRT = { lat: 1.3157320708493883, lng: 103.7650341376623 };
+const queenstownMRT = { lat: 1.2949046236465185, lng: 103.80583738778117 };
+const brasBasahMRT = { lat: 1.2977384060268324, lng: 103.85050163878333 };
+
+// Function to get location data by its name
+function getLocationData(locationName) {
+  const allLocations = [].concat(
+    buonaVistaData,
+    clementiData,
+    queenstownData,
+    brasBasahData
+  );
+  return allLocations.find(
+    (location) => location.locationName === locationName
+  );
+}
+// Function to load the Google Maps API script
+
+// Initialize the Google Map
+function initMap() {
+  // Code for Singapore lat lng
+  const centerMapSG = { lat: 1.3585778, lng: 103.8271599 };
+  const mapOptions = {
+    center: centerMapSG,
+    zoom: 12,
+  };
+  // Code for custom marker
+  newMarker = {
+    url: "./images/foodmap.png", // URL to the custom marker image
+    scaledSize: new google.maps.Size(30, 30), // Scaled size of the icon (width, height)};
+  };
+
+  // Initialize the map
+  map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+}
+
+// Add marker functions for each MRT station
+function displayBuonaVistaInfo() {
+  clearFoodOptions();
+  displayLocationNames(buonaVistaData, buonaVistaMRT);
+  addMarkers(buonaVistaData, buonaVistaMRT);
+}
+function adisplayClementiInfo() {
+  clearFoodOptions();
+  displayLocationNames(clementiData, clementiMRT);
+  addMarkers(clementiData, clementiMRT);
+}
+function displayQueenstownInfo() {
+  clearFoodOptions();
+  displayLocationNames(queenstownData, queenstownMRT);
+  addMarkers(queenstownData, queenstownMRT);
+}
+function displayBrasBasahInfo() {
+  clearFoodOptions();
+  displayLocationNames(brasBasahData, brasBasahMRT);
+  addMarkers(brasBasahData, brasBasahMRT);
+}
+
+// Add markers to the map and display location information
+function addMarkers(markerData, mrtCoordinates) {
+  // Remove previous markers from the map
+  clearMarkers();
+  // Track the currently open info window
+  let openInfoWindow = null;
+  const bounds = new google.maps.LatLngBounds();
+
+  // Iterate over marker data
+  markerData.forEach((data) => {
+    const position = { lat: data.lat, lng: data.lng };
+    const markerOptions = {
+      position: position,
+      map: map,
+      icon: newMarker,
+      optimized: false,
+      animation: google.maps.Animation.DROP,
+    };
+
+    // Code to show marker
+    const marker = new google.maps.Marker(markerOptions);
+    // Extend the bounds to include the marker's position
+    bounds.extend(marker.getPosition());
+    // Store the marker in the markers array
+    markers.push(marker);
+
+    // Code to calculate dist from MRT stations
+    const distance = calculateDistance(
+      mrtCoordinates.lat,
+      mrtCoordinates.lng,
+      data.lat,
+      data.lng
+    );
+
+    // Create info window content based on marker data
+    const content = `
+    <div class="info-window-content" style="font-family: 'Montserrat', sans-serif; color: #3C3633">
+        <h4>${data.locationName}</h4>
+        <p>${data.address}</p>
+    </div>
+`;
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: content,
+      minWidth: 250,
+      maxWidth: 300,
+    });
+
+    // Event listener for marker click
+    marker.addListener("click", function () {
+      // Close the previously opened info window, if any
+      if (openInfoWindow) {
+        openInfoWindow.close();
+      }
+      // Open the current info window
+      infoWindow.open(map, marker);
+      // Update the currently open info window
+      openInfoWindow = infoWindow;
+    });
+  });
+
+  // Fit the map to the bounds of the markers
+  map.fitBounds(bounds);
+}
+
+// Display location names above Google Maps
+function displayLocationNames(markerData, mrtCoordinates) {
+  const locationNamesContainer = document.getElementById("location-names");
+  locationNamesContainer.innerHTML = ""; // Clear previous location names
+
+  // Create a flex container for location cards
+  const locationCardsContainer = document.createElement("div");
+  locationCardsContainer.classList.add("location-cards-container");
+
+  // Create a card for each location
+  markerData.forEach((data) => {
+    const card = document.createElement("div");
+    card.classList.add("location-card");
+
+    // Calculate distance from MRT
+    const distance = calculateDistance(
+      mrtCoordinates.lat,
+      mrtCoordinates.lng,
+      data.lat,
+      data.lng
+    );
+
+    // Construct card content
+    const content = `
+      <div class="location-card-content location-card-content-small">
+      <img src="${data.image}" alt="${
+      data.locationName
+    }" class="location-image">
+        <h4>${data.locationName}</h4>
+        <p><strong>Address:</strong> ${data.address}</p>
+        <p><strong>Distance from MRT:</strong> ${distance.toFixed(0)} metres</p>
+        <button class="show-food-options-btn" data-location="${
+          data.locationName
+        }">Show Food Options</button>
+        <p><a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+          data.locationName
+        )}" target="_blank" class="view-map-link">View on Google Maps</a></p>
+        <p class="location-category ${getCategoryClass(data.category)}">${
+      data.category
+    }</p>
+      </div>
+    `;
+
+    card.innerHTML = content;
+
+    // Add event listener to show food options button
+    const showFoodOptionsBtn = card.querySelector(".show-food-options-btn");
+    showFoodOptionsBtn.addEventListener("click", function () {
+      showFoodOptions(this.getAttribute("data-location"));
+    });
+    locationCardsContainer.appendChild(card);
+  });
+  // Append the location cards container to the main container
+  locationNamesContainer.appendChild(locationCardsContainer);
+}
+
+// Function to display food options for a location
+function showFoodOptions(locationName) {
+  const locationData = getLocationData(locationName);
+  const foodOptionsDiv = document.getElementById("food-options");
+  if (locationData && locationData.foodOptions) {
+    // Clear previous food options
+    foodOptionsDiv.innerHTML = "";
+    // Create container for food options
+    const foodOptionsContainer = document.createElement("div");
+    foodOptionsContainer.classList.add("food-options-container");
+    // Create cards for each food option
+    locationData.foodOptions.forEach((option) => {
+      const card = document.createElement("div");
+      card.classList.add("food-option-card");
+      // Parse the food stall name, type of food stall, and type of cuisine
+      const [food_name, establishment_type, cuisine] = option.split(" - ");
+      card.innerHTML = `
+        <h4>${food_name}</h4>
+        <p><strong>Establishment Type:</strong> ${establishment_type}</p>
+        <p><strong>Cuisine:</strong> ${cuisine}</p>
+      `;
+      foodOptionsDiv.appendChild(card);
+    });
+  } else {
+    // If no food options available, display a message
+    foodOptionsDiv.innerHTML =
+      "<p>No food options available for " + locationName + "</p>";
+  }
+}
+// Function to clear food options
+function clearFoodOptions() {
+  const foodOptionsDiv = document.getElementById("food-options");
+  foodOptionsDiv.innerHTML = "";
+}
+
+// Clear markers from the map
+function clearMarkers() {
+  // Iterate over the markers array and remove each marker from the map
+  markers.forEach((marker) => {
+    marker.setMap(null);
+  });
+  // Empty the markers array
+  markers = [];
+}
+
+// Get CSS class based on category
+function getCategoryClass(category) {
+  switch (category) {
+    case "Shopping Mall":
+      return "shopping-mall";
+    case "Hotel":
+      return "hotel";
+    case "Business Center":
+      return "business-center";
+    default:
+      return "hawker-centre";
+  }
+}
+
+// Calculate distance between two coordinates
+function calculateDistance(lat1, lng1, lat2, lng2) {
+  const toRad = (value) => (value * Math.PI) / 180;
+  const R = 6371000; // Earth's radius in meters
+  const dLat = toRad(lat2 - lat1);
+  const dLng = toRad(lng2 - lng1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
