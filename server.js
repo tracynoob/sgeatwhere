c5a413683a7bfb7fbf7c4f3d6730b143a1b7fadd
tracynoob <162657107+tracynoob@users.noreply.github.com>
2024-06-02 14:10:10 +0800

    add auth callback path

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/auth/signin", (req, res) => {
   const { data, error } = supabase.auth.signInWithOAuth({
     provider: provider,
     options: {
-      redirectTo: "https://sgeatwhere.onrender.com", // redirectTo landing page
+      redirectTo: "https://sgeatwhere.onrender.com/auth/callback", // redirectTo landing page
     },
   });
 
